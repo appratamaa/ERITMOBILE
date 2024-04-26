b65d16c63d77b297baf86d43dc40b5c8105c1f8b
appratamaa <andreputrap30@gmail.com>
2024-04-26 14:44:56 +0700

    Activity Landing Screen Cyan APP

--- app/src/main/java/org/bumandhala/eritmobile/MainActivity.kt
+++ app/src/main/java/org/bumandhala/eritmobile/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = Color.Blue
+                    color = Color.Cyan
                 ) {
                     Greeting("")
                 }
