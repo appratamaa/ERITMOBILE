71f2f986af5b740491e2b9a15dad2b6466c05ee0
appratamaa <andreputrap30@gmail.com>
2024-04-26 22:24:37 +0700

    Landing Screen 1 Blue APP

--- app/src/main/java/org/bumandhala/eritmobile/ui/screen/LandingScreen.kt
+++ app/src/main/java/org/bumandhala/eritmobile/ui/screen/LandingScreen.kt
@@ -1,2 +1,47 @@
 package org.bumandhala.eritmobile.ui.screen
 
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.bumandhala.eritmobile.R
+import org.bumandhala.eritmobile.ui.theme.ERITMOBILETheme
+
+@Composable
+fun LandingScreen(content: @Composable () -> Unit) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFF1A237E))
+    ) {
+        Box(
+            modifier = Modifier.align(Alignment.Center)
+        ) {
+            Image(
+                painter = painterResource(R.drawable.logoertitmobile),
+                contentDescription = null,
+                modifier = Modifier.size(200.dp)
+            )
+        }
+    }
+}
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun ScreenPreview() {
+    ERITMOBILETheme {
+        LandingScreen {
+        }
+    }
+}
